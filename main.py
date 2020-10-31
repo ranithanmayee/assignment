aace3ade891e8c159d92e7b9de49979e9904a5f1
ranithanmayee <57262430+ranithanmayee@users.noreply.github.com>
2020-10-31 19:29:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+myDict = {
+    "Fitbit Plus": 7980,
+    "IPods": 22349,
+    "MI Band": 999,
+    "Cult Pass": 2799,
+    "Macbook Pro": 229900,
+    "Digital Camera": 11101,
+    "Alexa": 9999,
+    "Sandwich Toaster": 2195,
+    "Microwave Oven": 9800,
+    "Scale": 4999
+}
+sorted = {k: v for k, v in sorted(myDict.items(), key=lambda item: item[1])}
+listt = zip(sorted.keys(), sorted.values()) 
+listt = list(listt) 
+
+
+numOfEmp = 2
+index = 0
+diff = listt[numOfEmp-1][1] - listt[0][1]
+i = 1
+while(i < len(listt) - numOfEmp+1):
+    if listt[i+numOfEmp-1][1] - listt[i][1] < diff:
+        index = i
+        diff = listt[i+numOfEmp-1][1] - listt[i][1]
+    i += 1
+
+
+print('Here the goodies that are selected for distribution are:')
+j = 0
+while(j < numOfEmp):
+    print(listt[index+j][0]+':', listt[index+j][1])
+    j += 1
+print()
+print('And the difference between the chosen goodie with highest price and the lowest price is
+', diff)
